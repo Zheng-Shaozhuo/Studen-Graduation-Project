b9cca259ac496a70d993e7643b7304fbe3098ea5
ZhengShaozhuo <Zheng-Shaozhuo@outlook.com>
2018-03-01 00:52:06 +0800

    qian台视图路径配置之插件路径修改

--- APP/Ucenter/Conf/config.php
+++ APP/Ucenter/Conf/config.php
@@ -5,7 +5,7 @@ return array(
 		'__JS__'     	=> __ROOT__ . '/Public/Styles/js', // 增加新的JS类库路径替换规则 
 		'__CSS__'     	=> __ROOT__ . '/Public/Styles/css', // 增加新的JS类库路径替换规则 
 		'__IMG__' 		=> __ROOT__ . '/Public/Images', // 增加新的上传路径替换规则
-		'__PLUGINS__' 		=> __ROOT__ . '/Public/Pugins', // 增加新的插件替换规则
+		'__PLUGINS__' 		=> __ROOT__ . '/Public/Plugins', // 增加新的插件替换规则
 	),
 	'LAYOUT_ON'			=> false,
 	'SHOW_PAGE_TRACE'	=> true,
