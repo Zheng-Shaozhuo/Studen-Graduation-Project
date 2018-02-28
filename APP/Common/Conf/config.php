be22e6c6a25ff78c905df26d38fdbd6d22387d75
ZhengShaozhuo <Zheng-Shaozhuo@outlook.com>
2018-02-28 23:51:25 +0800

    提交应用数据库配置文件 conf，用于关联数据访问

---
+++ APP/Common/Conf/config.php
@@ -0,0 +1,11 @@
+<?php
+return array(
+	'DB_TYPE'   => 'mysql', // 数据库类型
+	'DB_HOST'   => 'localhost', // 服务器地址
+	'DB_NAME'   => 'gproject', // 数据库名
+	'DB_USER'   => 'root', // 用户名
+	'DB_PWD'    => 'root', // 密码
+	'DB_PORT'   => 3306, // 端口
+	'DB_PREFIX' => '', // 数据库表前缀 
+	'DB_CHARSET'=> 'utf8', // 字符集	
+);
