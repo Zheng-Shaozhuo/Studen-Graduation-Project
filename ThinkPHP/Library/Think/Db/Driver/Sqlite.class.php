1e002da62d3999bd89d0b30b89d85acfd6321e66
ZhengShaozhuo <Zheng-Shaozhuo@outlook.com>
2018-03-01 00:39:25 +0800

    thinkphp3.2 框架内核文件补全(核心版更换为完全版)

--- ThinkPHP/Library/Think/Db/Driver/Sqlite.class.php
+++ ThinkPHP/Library/Think/Db/Driver/Sqlite.class.php
@@ -11,25 +11,27 @@
 namespace Think\Db\Driver;
 use Think\Db;
 defined('THINK_PATH') or exit();
-
 /**
- * Mysql数据库驱动类
+ * Sqlite数据库驱动
  */
-class Mysql extends Db{
+class Sqlite extends Db {
 
     /**
      * 架构函数 读取数据库配置信息
      * @access public
      * @param array $config 数据库配置数组
      */
-    public function __construct($config=''){
-        if ( !extension_loaded('mysql') ) {
-            E(L('_NOT_SUPPERT_').':mysql');
+    public function __construct($config='') {
+        if ( !extension_loaded('sqlite') ) {
+            E(L('_NOT_SUPPERT_').':sqlite');
         }
         if(!empty($config)) {
-            $this->config   =   $config;
+            if(!isset($config['mode'])) {
+                $config['mode']	=	0666;
+            }
+            $this->config	=	$config;
             if(empty($this->config['params'])) {
-                $this->config['params'] =   '';
+                $this->config['params'] =   array();
             }
         }
     }
@@ -37,29 +39,15 @@ class Mysql extends Db{
     /**
      * 连接数据库方法
      * @access public
-     * @throws ThinkExecption
      */
-    public function connect($config='',$linkNum=0,$force=false) {
+    public function connect($config='',$linkNum=0) {
         if ( !isset($this->linkID[$linkNum]) ) {
-            if(empty($config))  $config =   $this->config;
-            // 处理不带端口号的socket连接情况
-            $host = $config['hostname'].($config['hostport']?":{$config['hostport']}":'');
-            // 是否长连接
+            if(empty($config))	$config	=	$this->config;
             $pconnect   = !empty($config['params']['persist'])? $config['params']['persist']:$this->pconnect;
-            if($pconnect) {
-                $this->linkID[$linkNum] = mysql_pconnect( $host, $config['username'], $config['password'],131072);
-            }else{
-                $this->linkID[$linkNum] = mysql_connect( $host, $config['username'], $config['password'],true,131072);
-            }
-            if ( !$this->linkID[$linkNum] || (!empty($config['database']) && !mysql_select_db($config['database'], $this->linkID[$linkNum])) ) {
-                E(mysql_error());
-            }
-            $dbVersion = mysql_get_server_info($this->linkID[$linkNum]);
-            //使用UTF8存取数据库
-            mysql_query("SET NAMES '".$config['charset']."'", $this->linkID[$linkNum]);
-            //设置 sql_model
-            if($dbVersion >'5.0.1'){
-                mysql_query("SET sql_mode=''",$this->linkID[$linkNum]);
+            $conn = $pconnect ? 'sqlite_popen':'sqlite_open';
+            $this->linkID[$linkNum] = $conn($config['database'],$config['mode']);
+            if ( !$this->linkID[$linkNum]) {
+                E(sqlite_error_string());
             }
         }
         return $this->linkID[$linkNum];
@@ -70,7 +58,6 @@ class Mysql extends Db{
      * @access public
      */
     public function free() {
-        mysql_free_result($this->queryID);
         $this->queryID = null;
     }
 
@@ -85,21 +72,17 @@ class Mysql extends Db{
         if ( !$this->_linkID ) return false;
         $this->queryStr = $str;
         //释放前次的查询结果
-        if ( $this->queryID ) {    $this->free();    }
+        if ( $this->queryID ) $this->free();
         N('db_query',1);
         // 记录开始执行时间
         G('queryStartTime');
-        $this->queryID = mysql_query($str, $this->_linkID);
+        $this->queryID = sqlite_query($this->_linkID,$str);
         $this->debug();
         if ( false === $this->queryID ) {
             $this->error();
             return false;
         } else {
-            if(0===stripos($str, 'call')){ // 存储过程查询支持
-                $this->close();
-                $this->linkID  = array();
-            }
-            $this->numRows = mysql_num_rows($this->queryID);
+            $this->numRows = sqlite_num_rows($this->queryID);
             return $this->getAll();
         }
     }
@@ -108,25 +91,25 @@ class Mysql extends Db{
      * 执行语句
      * @access public
      * @param string $str  sql指令
-     * @return integer|false
+     * @return integer
      */
     public function execute($str) {
         $this->initConnect(true);
         if ( !$this->_linkID ) return false;
         $this->queryStr = $str;
         //释放前次的查询结果
-        if ( $this->queryID ) {    $this->free();    }
+        if ( $this->queryID ) $this->free();
         N('db_write',1);
         // 记录开始执行时间
         G('queryStartTime');
-        $result =   mysql_query($str, $this->_linkID) ;
+        $result	=	sqlite_exec($this->_linkID,$str);
         $this->debug();
-        if ( false === $result) {
+        if ( false === $result ) {
             $this->error();
             return false;
         } else {
-            $this->numRows = mysql_affected_rows($this->_linkID);
-            $this->lastInsID = mysql_insert_id($this->_linkID);
+            $this->numRows = sqlite_changes($this->_linkID);
+            $this->lastInsID = sqlite_last_insert_rowid($this->_linkID);
             return $this->numRows;
         }
     }
@@ -141,7 +124,7 @@ class Mysql extends Db{
         if ( !$this->_linkID ) return false;
         //数据rollback 支持
         if ($this->transTimes == 0) {
-            mysql_query('START TRANSACTION', $this->_linkID);
+            sqlite_query($this->_linkID,'BEGIN TRANSACTION');
         }
         $this->transTimes++;
         return ;
@@ -154,12 +137,12 @@ class Mysql extends Db{
      */
     public function commit() {
         if ($this->transTimes > 0) {
-            $result = mysql_query('COMMIT', $this->_linkID);
-            $this->transTimes = 0;
+            $result = sqlite_query($this->_linkID,'COMMIT TRANSACTION');
             if(!$result){
                 $this->error();
                 return false;
             }
+            $this->transTimes = 0;
         }
         return true;
     }
@@ -171,12 +154,12 @@ class Mysql extends Db{
      */
     public function rollback() {
         if ($this->transTimes > 0) {
-            $result = mysql_query('ROLLBACK', $this->_linkID);
-            $this->transTimes = 0;
+            $result = sqlite_query($this->_linkID,'ROLLBACK TRANSACTION');
             if(!$result){
                 $this->error();
                 return false;
             }
+            $this->transTimes = 0;
         }
         return true;
     }
@@ -190,10 +173,11 @@ class Mysql extends Db{
         //返回数据集
         $result = array();
         if($this->numRows >0) {
-            while($row = mysql_fetch_assoc($this->queryID)){
-                $result[]   =   $row;
+            for($i=0;$i<$this->numRows ;$i++ ){
+                // 返回数组集
+                $result[$i] = sqlite_fetch_array($this->queryID,SQLITE_ASSOC);
             }
-            mysql_data_seek($this->queryID,0);
+            sqlite_seek($this->queryID,0);
         }
         return $result;
     }
@@ -204,14 +188,14 @@ class Mysql extends Db{
      * @return array
      */
     public function getFields($tableName) {
-        $result =   $this->query('SHOW COLUMNS FROM '.$this->parseKey($tableName));
+        $result =   $this->query('PRAGMA table_info( '.$tableName.' )');
         $info   =   array();
-        if($result) {
+        if($result){
             foreach ($result as $key => $val) {
                 $info[$val['Field']] = array(
                     'name'    => $val['Field'],
                     'type'    => $val['Type'],
-                    'notnull' => (bool) (strtoupper($val['Null']) === 'NO'), // not null is empty, null is yes
+                    'notnull' => (bool) ($val['Null'] === ''), // not null is empty, null is yes
                     'default' => $val['Default'],
                     'primary' => (strtolower($val['Key']) == 'pri'),
                     'autoinc' => (strtolower($val['Extra']) == 'auto_increment'),
@@ -227,12 +211,9 @@ class Mysql extends Db{
      * @return array
      */
     public function getTables($dbName='') {
-        if(!empty($dbName)) {
-           $sql    = 'SHOW TABLES FROM '.$dbName;
-        }else{
-           $sql    = 'SHOW TABLES ';
-        }
-        $result =   $this->query($sql);
+        $result =   $this->query("SELECT name FROM sqlite_master WHERE type='table' "
+             . "UNION ALL SELECT name FROM sqlite_temp_master "
+             . "WHERE type='table' ORDER BY name");
         $info   =   array();
         foreach ($result as $key => $val) {
             $info[$key] = current($val);
@@ -241,59 +222,12 @@ class Mysql extends Db{
     }
 
     /**
-     * 替换记录
-     * @access public
-     * @param mixed $data 数据
-     * @param array $options 参数表达式
-     * @return false | integer
-     */
-    public function replace($data,$options=array()) {
-        foreach ($data as $key=>$val){
-            $value   =  $this->parseValue($val);
-            if(is_scalar($value)) { // 过滤非标量数据
-                $values[]   =  $value;
-                $fields[]     =  $this->parseKey($key);
-            }
-        }
-        $sql   =  'REPLACE INTO '.$this->parseTable($options['table']).' ('.implode(',', $fields).') VALUES ('.implode(',', $values).')';
-        return $this->execute($sql);
-    }
-
-    /**
-     * 插入记录
-     * @access public
-     * @param mixed $datas 数据
-     * @param array $options 参数表达式
-     * @param boolean $replace 是否replace
-     * @return false | integer
-     */
-    public function insertAll($datas,$options=array(),$replace=false) {
-        if(!is_array(reset($datas))) return false;
-        $fields = array_keys($datas[0]);
-        array_walk($fields, array($this, 'parseKey'));
-        $values  =  array();
-        foreach ($datas as $data){
-            $value   =  array();
-            foreach ($data as $key=>$val){
-                $val   =  $this->parseValue($val);
-                if(is_scalar($val)) { // 过滤非标量数据
-                    $value[]   =  $val;
-                }
-            }
-            $values[]    = '('.implode(',', $value).')';
-        }
-        $sql   =  ($replace?'REPLACE':'INSERT').' INTO '.$this->parseTable($options['table']).' ('.implode(',', $fields).') VALUES '.implode(',',$values);
-        return $this->execute($sql);
-    }
-
-    /**
      * 关闭数据库
      * @access public
-     * @return void
      */
     public function close() {
         if ($this->_linkID){
-            mysql_close($this->_linkID);
+            sqlite_close($this->_linkID);
         }
         $this->_linkID = null;
     }
@@ -305,7 +239,8 @@ class Mysql extends Db{
      * @return string
      */
     public function error() {
-        $this->error = mysql_errno().':'.mysql_error($this->_linkID);
+        $code   =   sqlite_last_error($this->_linkID);
+        $this->error = $code.':'.sqlite_error_string($code);
         if('' != $this->queryStr){
             $this->error .= "\n [ SQL语句 ] : ".$this->queryStr;
         }
@@ -316,30 +251,28 @@ class Mysql extends Db{
     /**
      * SQL指令安全过滤
      * @access public
-     * @param string $str  SQL字符串
+     * @param string $str  SQL指令
      * @return string
      */
     public function escapeString($str) {
-        if($this->_linkID) {
-            return mysql_real_escape_string($str,$this->_linkID);
-        }else{
-            return mysql_escape_string($str);
-        }
-        // return $str;
+        return sqlite_escape_string($str);
     }
 
-
     /**
-     * 字段和表名处理添加`
-     * @access protected
-     * @param string $key
+     * limit
+     * @access public
      * @return string
      */
-    protected function parseKey(&$key) {
-        $key   =  trim($key);
-        if(!is_numeric($key) && !preg_match('/[,\'\"\*\(\)`.\s]/',$key)) {
-           $key = '`'.$key.'`';
+    public function parseLimit($limit) {
+        $limitStr    = '';
+        if(!empty($limit)) {
+            $limit  =   explode(',',$limit);
+            if(count($limit)>1) {
+                $limitStr .= ' LIMIT '.$limit[1].' OFFSET '.$limit[0].' ';
+            }else{
+                $limitStr .= ' LIMIT '.$limit[0].' ';
+            }
         }
-        return $key;
+        return $limitStr;
     }
 }
